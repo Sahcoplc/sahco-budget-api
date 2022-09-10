4feef6eb808558025338baeb1efb7add036db670
Gbemisola Kotoye <barkty@yahoo.com>
2022-09-10 17:21:19 +0100

    cors

--- app/index.js
+++ app/index.js
@@ -28,7 +28,13 @@ const corsOptions = {
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH', 'OPTIONS'],
     credentials: true
 };
-app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, UPDATE, PUT, PATCH, OPTIONS, HEAD");
+    next();
+});
+// app.use(cors(corsOptions));
 app.use(morgan("tiny"));
 app.use(express.json());
 
