fc57c7fc67c7195f304bcce790720ee1f1fb2c9a
Gbemisola Kotoye <barkty@yahoo.com>
2022-10-09 23:40:58 +0100

    fix(authentication): Add authentication middleware

--- app/routes/User.js
+++ app/routes/User.js
@@ -7,22 +7,16 @@ const router = express.Router()
 import authMiddleWare from '../middlewares/auth.js'
 import imageUpload from "../middlewares/uploads/imageUpload.js";
 
-// router.post('/new', authMiddleWare, createUser)
-// router.get('/all', authMiddleWare, getUsers)
-// router.get('/:id', authMiddleWare, getUser)
-// router.patch('/update', authMiddleWare, imageUpload.array('avatar'), updatedUser)
-// router.delete('/:id', authMiddleWare, deleteUser)
-
 //Logged in user
 // router.get('/user/profile', authMiddleWare, getProfile)
 
 const userControl = new UsersController()
 
-router.post('/new', userControl.createUser)
-router.get('/all', userControl.findUsers)
-router.get('/:id', userControl.findUser)
-router.patch('/update', imageUpload.array('avatar'), userControl.updateUser)
-router.delete('/:id', userControl.deleteUser)
+router.post('/new', authMiddleWare, userControl.createUser)
+router.get('/all', authMiddleWare, userControl.findUsers)
+router.get('/:id',authMiddleWare, userControl.findUser)
+router.patch('/update', authMiddleWare, imageUpload.array('avatar'), userControl.updateUser)
+router.delete('/:id', authMiddleWare, userControl.deleteUser)
 
 
 export default router;
