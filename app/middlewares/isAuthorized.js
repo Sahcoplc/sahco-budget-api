033a90da86f9c351bb4f05a627cc6f5db50a1217
Gbemisola Kotoye <barkty@yahoo.com>
2023-10-13 10:44:45 +0100

    update middleware

--- app/middlewares/isAuthorized.js
+++ app/middlewares/isAuthorized.js
@@ -3,37 +3,23 @@ import { error } from "../helpers/response";
 export const isAuthorized = (requiredPermissions = [], requiredLocations = [], requiredDepts = []) =>
   async function checkAuthorization(req, res, next) {
     try {
-        if (!req.user) return error(res, 400, "You are not logged in");
-        const { currentStation: { parent },permissions: { isController, actions, stations, departments } } = req.user;
+      if (!req.user) return error(res, 400, "You are not logged in");
+      const { permissions: { actions, stations, departments, modules } } = req.user;
+      const module = modules.find((module) => module.section === 'sahco-budget')
+      
+      if (actions.length === 0 || stations.length === 0 || departments.length === 0 || !module) {
+        return error(res, 401, "You are not authorized")
+      }
+      
+      const isOpenToAll = requiredPermissions.length === 0;
+      const isOpenToAllLocation = requiredLocations.length === 0;
+      const isOpenToAllDept = requiredDepts.length === 0;
+      
+      const userHasPermission = actions.some((action) => requiredPermissions.includes(action.name)) || isOpenToAll
+      const userHasLocation = stations.some((action) => requiredLocations.includes(action.code)) || isOpenToAllLocation
+      const userHasDept = departments.some((dept) => requiredDepts.includes(dept.name)) || isOpenToAllDept
 
-        const isOpenToAll = requiredPermissions.length === 0;
-        const isOpenToAllLocation = requiredLocations.length === 0;
-        const isOpenToAllDept = requiredDepts.length === 0;
-
-        if (actions.length === 0 || stations.length === 0 || departments.length === 0) {
-            return error(res, 401, "You are not authorized");
-        }
-
-        const userHasPermission = actions.some((action) => requiredPermissions.includes(action.name));
-        const userHasLocation = stations.some((action) => requiredLocations.includes(action.code));
-        const userHasDept = departments.some((dept) => requiredDepts.includes(dept.name));
-
-        if (isController && parent) {
-            req.user.role = isController;
-            return next();
-        }
-
-        if (isOpenToAll && isOpenToAllDept && isOpenToAllLocation) return next();
-
-        if (userHasPermission && userHasLocation && userHasDept) {
-            req.user.role = isController;
-            return next();
-        }
-
-        if (isOpenToAllLocation && userHasPermission && userHasDept) {
-            req.user.role = isController;
-            return next();
-        }
+      if (userHasPermission && userHasLocation && userHasDept) return next()
 
       return error(res, 401, "You are not authorized");
     } catch (err) {
--- app/middlewares/rateLimit.js
+++ app/middlewares/rateLimit.js
@@ -1,16 +1,29 @@
 import toobusy from 'toobusy-js';
 import rateLimit from 'express-rate-limit';
+import MongoStore from "rate-limit-mongo";
+
+const { DATABASE_URL, SESSION_DB_NAME, SESSION_DB_COLLECTION, NODE_ENV } = process.env;
+
+const options = {
+    uri: DATABASE_URL,
+    authSource: SESSION_DB_NAME,
+    collectionName: SESSION_DB_COLLECTION,
+    expireTimeMs: 15 * 60 * 1000,
+    errorHandler: console.error.bind(null, 'rate-limit-mongo')
+    // see Configuration section for more options and details
+}
 
 export const rateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     message: 'Too many requests created from this IP, please try again after an hour',
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    store: new MongoStore(options),
 })
 
 export const apiBusy = (req, res, next) => {
-    if (toobusy()) {
+    if (NODE_ENV !== 'test' && toobusy()) {
         res.status(503).send("Server Too Busy");
     } else {
         next();
